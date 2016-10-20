87c31f367a2f12fcf54d71de7bf3785a6842a847
Ken <xynoci.k@gmail.com>
2016-10-20 19:55:40 -0400

    add mocks.

--- src/test/java/hw2/VisitorListGeneratorTest.java
+++ src/test/java/hw2/VisitorListGeneratorTest.java
@@ -8,6 +8,11 @@ import java.util.Random;
 
 import citysim9002.base.Visitor;
 import citysim9002.service.VisitorListGenerator;
+import javafx.scene.control.RadioMenuItem;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Xynoci on 10/5/16.
@@ -21,10 +26,14 @@ public class VisitorListGeneratorTest {
      */
     @Test
     public void isOneTypeFromTheFour() throws Exception {
+        Random mockRandom = mock(Random.class);
+        when(mockRandom.nextInt(any(int.class))).thenReturn(9);
+        VisitorListGenerator mockVisitorListGenerator = new VisitorListGenerator(mockRandom.nextInt());
+
         boolean flag = true;
         Validator validator = new Validator();
-        LinkedList<Visitor> vislist = visgen.getVisitorList();
-        for (Visitor aVisitor : vislist) {
+        LinkedList<Visitor> visitorList = mockVisitorListGenerator.getVisitorList();
+        for (Visitor aVisitor : visitorList) {
             if (!validator.isOneTypeFromTheFourValidator(aVisitor)) {
                 flag = false;
                 break;
@@ -39,8 +48,12 @@ public class VisitorListGeneratorTest {
      */
     @Test
     public void isThereFiveVisitorsGenerated() throws Exception {
+        Random mockRandom = mock(Random.class);
+        when(mockRandom.nextInt(any(int.class))).thenReturn(6);
+        VisitorListGenerator mockVisitorListGenerator = new VisitorListGenerator(mockRandom.nextInt());
+
         int numberOfVisitorsExpected = 5;
-        int numberOfVisitorsGenerated = visgen.getVisitorList().size();
+        int numberOfVisitorsGenerated = mockVisitorListGenerator.getVisitorList().size();
         Assert.assertEquals(numberOfVisitorsExpected, numberOfVisitorsGenerated);
     }
 }
